821ebbdd7dcbc25a3a417652403c6ada4876b6d3
Fulton Garcia <fulton@geniusplaza.com>
2020-02-10 20:03:46 -0500

    main layout and leaflet

---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { MainContainer, MapContainer, ListContainer } from '../styles';
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+
+const MainPage = () => {
+  return (
+    <MainContainer>
+      <MapContainer>
+        <LeafletMap center={[25.7601, -80.3744]} zoom={18}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={[25.7601, -80.3744]}>
+            <Popup>
+              <div>
+                <h2>FIU Parking Garage 6</h2>
+              </div>
+            </Popup>
+          </Marker>
+        </LeafletMap>
+      </MapContainer>
+      <ListContainer></ListContainer>
+    </MainContainer>
+  )
+};
+
+export default MainPage;
---
+++ src/Components/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`;
+
+export const MapContainer = styled.div`
+  width: 75%;
+`;
+
+export const ListContainer = styled.div`
+  width: 25%;
+`;
