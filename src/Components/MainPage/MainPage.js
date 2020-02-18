562a80b69a226584b870a398adb1773cc4c5075e
Fulton Garcia <fulton@geniusplaza.com>
2020-02-17 19:50:35 -0500

    moved map to own component

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { MainContainer, MapContainer, ListContainer } from './styles';
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 import RouteCard from "../MainPage/RouteCard";
 import API from "../../Services/API";
+import MapComponent from './Map';
 
 const MainPage = () => {
 
@@ -20,19 +20,7 @@ const MainPage = () => {
   return (
     <MainContainer>
       <MapContainer>
-        <LeafletMap center={[25.7601, -80.3744]} zoom={18}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          />
-          <Marker position={[25.7601, -80.3744]}>
-            <Popup>
-              <div>
-                <h2>FIU Parking Garage 6</h2>
-              </div>
-            </Popup>
-          </Marker>
-        </LeafletMap>
+        <MapComponent/>
       </MapContainer>
       <ListContainer>
         <RouteCard title={"Routes"} routes={routes}/>
