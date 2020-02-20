cc669e41f089d0f2c381a2aec3e58e71d04b6492
Help <pczerokey@gmail.com>
2020-02-19 19:44:55 -0500

    convered ListCard from functional component to Class Component

--- src/Components/MainPage/ListCard/index.js
+++ src/Components/MainPage/ListCard/index.js
@@ -3,19 +3,32 @@ import Card from '../../Commons/Card/Card';
 import { Title } from './styles';
 import API from '../../../Services/API';
 
-const ListCard = (props) => {
 
-  const handleRouteClick = async (item) => {
-    let res = await API.getInfoByRoute(item.route_id);
-    console.log(res);
+const handleRouteClick = async (item) => {
+  let res = await API.getInfoByRoute(item.route_id);
+  console.log(res);
+}
+
+class ListCard extends React.Component
+{
+  constructor(props) {
+    super(props);
   }
 
-  return (
-    <>
-      <Title>{props.title}</Title>
-      {props.routes.map((item, i) => <Card height="10%" margin="5px" key={i} onClick={() => handleRouteClick(item)}>{item.route_long_name}</Card>)}
-    </>
-  )
+  render() {
+    return(
+        <>
+          <Title>{this.props.title}</Title>
+          {
+            this.props.routes.map(
+                (item, i) =>
+                    //Display Routes
+                    <Card height="10%" margin="5px" key={i} onClick={() => handleRouteClick(item)}>{item.route_long_name}</Card>
+            )
+          }
+        </>
+    )
+  }
 }
 
 export default ListCard;
