871acde7d320b12746ff5f2604c29f6c8de437c0
Help <pczerokey@gmail.com>
2020-02-25 01:38:12 -0500

    ListCard component now has state to render routes or trips

--- src/Components/MainPage/ListCard/index.js
+++ src/Components/MainPage/ListCard/index.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Title} from './styles';
 import RouteCard from "../RouteCard";
 
 let passedRouteId = null;
 
-function getRouteID(routeID) {
-    passedRouteId = routeID;
-    console.log("THE ID PASSED SUCCESSFULLY to ListCard", passedRouteId);
-}
+const ListCard = (props) => {
 
-const ListCard = ({title, routes}) => {
+    //This needs to get implemented
+    const [trips, setTrps] = useState("NoTRIPS");
+    const [routes, setRoutes] = useState("NoROUTES");
+
+    function getRouteID(routeID) {
+        passedRouteId = routeID;
+        setRoutes(routeID);
+        console.log("THE ID PASSED SUCCESSFULLY to ListCard", passedRouteId);
+        console.log("routes State var is now ", routes);
+    }
+
+    useEffect(
+        () => {
+            console.log("ListCard reloaded with routes being", routes);
+        },
+        [routes],
+    );
+    //Got discontinued
+    // useEffect(
+    //     () => {
+    //         const subscription = props.source.subscribe();
+    //         return () => {
+    //             subscription.unsubscribe();
+    //         };
+    //     },
+    //     [props.source],
+    // );
+    
     return (
         <>
             {/* Displays Routes */}
-            <Title>{title}</Title>
+            <Title>{props.title}</Title>
 
-            <RouteCard Routes={routes} callBack={getRouteID}/>
+            <RouteCard Routes={props.routes} callBack={getRouteID}/>
             {/*  TODO add trips - don't know how :D*/}
         </>
     )
