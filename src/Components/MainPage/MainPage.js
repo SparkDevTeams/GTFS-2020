fb1d82f60b46d2052344b79909626fc2ddef0be2
Help <pczerokey@gmail.com>
2020-02-25 20:34:02 -0500

    Added trips and shapes to map function, needs fulton review

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -6,11 +6,20 @@ import MapComponent from './Map';
 
 //TODO Need fulton to approve or use stateVariables
 let routeID = "";
+let tripsWithRoutes
 const MainPage = () => {
+
+    //TODO ask how to get rid of let
+    let getTripsWithRoutes = async (routeId) => {
+        tripsWithRoutes = await API.getInfoByRoute(routeId);
+        console.log("GETINFOBYROUTES",tripsWithRoutes);
+    };
+
 
     function passRoute(newRouteID){
         routeID = newRouteID;
         console.log("Main page now has a new RouteID", routeID);
+        getTripsWithRoutes(newRouteID);
     }
 
     const [routes, setRoutes] = useState([]);
@@ -27,7 +36,9 @@ const MainPage = () => {
     return (
         <MainContainer>
             <MapContainer>
-                <MapComponent/>
+                <MapComponent
+                    trips={tripsWithRoutes}
+                />
             </MapContainer>
             <ListContainer>
                 <ListCard title={"Routes"} routes={routes} passRouteId={passRoute}/>
--- src/Components/MainPage/RouteCard/index.js
+++ src/Components/MainPage/RouteCard/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card from "../../Commons/Card/Card";
 import API from "../../../Services/API";
 
 
 class RouteCard extends React.Component {
 
+
+
     handleRouteClick = async (item) => {
         this.props.callBack(item.route_id);
-        let res = await API.getInfoByRoute(item.route_id);
-        console.log("GETINFOBYROUTES",res);
+        //Is Being done in MainPage
+        // let res = await API.getInfoByRoute(item.route_id);
+        // console.log("GETINFOBYROUTES in RouteCard",res);
     };
+
 
 
     render() {
@@ -21,6 +25,9 @@ class RouteCard extends React.Component {
                         <Card height="10%" margin="5px" key={i}
                               onClick={() => this.handleRouteClick(route)}>
                             {route.route_long_name}
+                            <ul>
+
+                            </ul>
                         </Card>
                     )
                 }
