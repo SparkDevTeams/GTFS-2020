40cf8ae29faa3afac82c6d1e5bd5e43c759856da
Fulton Garcia <fultongarcia@gmail.com>
2020-04-04 17:43:47 -0400

    panning to route

--- src/Components/MainPage/Map/index.js
+++ src/Components/MainPage/Map/index.js
@@ -1,34 +1,37 @@
-import React, {useEffect, useState} from 'react';
-import { Map as LeafletMap, TileLayer, CircleMarker, Popup, Polyline } from 'react-leaflet';
+import React, { useEffect, useState, useRef } from "react";
+import {
+  Map as LeafletMap,
+  TileLayer,
+  CircleMarker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
 
 const MapComponent = (props) => {
+  const [shape, setShape] = useState([]);
+  const mapRef = useRef();
 
-    const [shape, setShape] = useState([]);
+  useEffect(() => {
+    setShape(props.shape);
+    if (props.shape?.RoutePath?.length) {
+      mapRef.current.leafletElement.panTo(props.shape?.RoutePath?.[Math.floor((props.shape?.RoutePath?.length - 1) / 2)]);
+    }
+  }, [props.shape]);
 
-    // useEffect(() => {
-    //     setShape(props.trips[0].shape_info);
-    // }, [props.trips]);
-
-    useEffect(() => {
-        setShape(props.shape)
-    }, [props.shape]);
-
-    return (
-        <LeafletMap center={[25.7617, -80.1918]} zoom={12}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-            />
-            <Polyline positions={shape?.RoutePath || []} color={shape?.LineColor}>
-                <Popup>
-                    {shape.RoutePath &&
-                        <div>{shape.Names}</div>
-                    }
-                </Popup>
-            </Polyline>
-            {shape?.stops?.map(mark => <CircleMarker radius={3} center={mark}></CircleMarker>)}
-        </LeafletMap>
-    )
+  return (
+    <LeafletMap center={[25.7617, -80.1918]} zoom={13} ref={mapRef}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+      />
+      <Polyline positions={shape?.RoutePath || []} color={shape?.LineColor}>
+        <Popup>{shape.RoutePath && <div>{shape.Names}</div>}</Popup>
+      </Polyline>
+      {shape?.stops?.map((mark) => (
+        <CircleMarker radius={3} center={mark}></CircleMarker>
+      ))}
+    </LeafletMap>
+  );
 };
 
 export default MapComponent;
