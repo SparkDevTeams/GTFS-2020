6ed1e3390724379536c0b4fa70df883278bd0bab
Fulton Garcia <fultongarcia@gmail.com>
2020-04-04 19:30:37 -0400

    geolocation done

--- src/Components/MainPage/Map/index.js
+++ src/Components/MainPage/Map/index.js
@@ -5,18 +5,30 @@ import {
   CircleMarker,
   Popup,
   Polyline,
+  Marker,
+  Circle,
 } from "react-leaflet";
 
+import Location from "../../Location/Location";
+
 const MapComponent = (props) => {
-  const [shape, setShape] = useState([]);
+  const [shape, setShape] = useState({});
+  const [currentLocation, setCurrentLocation] = useState(undefined);
   const mapRef = useRef();
 
   useEffect(() => {
     setShape(props.shape);
     if (props.shape?.RoutePath?.length) {
-      mapRef.current.leafletElement.panTo(props.shape?.RoutePath?.[Math.floor((props.shape?.RoutePath?.length - 1) / 2)]);
+      let middle = Math.floor((props.shape?.RoutePath?.length - 1) / 2);
+      mapRef.current.leafletElement.panTo(props.shape?.RoutePath?.[middle]);
     }
   }, [props.shape]);
+
+  useEffect(() => {
+    if(currentLocation?.position){
+      mapRef.current.leafletElement.panTo(currentLocation.position);
+    }
+  }, [currentLocation]);
 
   return (
     <LeafletMap center={[25.7617, -80.1918]} zoom={13} ref={mapRef}>
@@ -24,6 +36,16 @@ const MapComponent = (props) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
       />
+      <Location setCurrentLocation={setCurrentLocation} />
+      {currentLocation?.position && (
+        <>
+          <Marker position={currentLocation.position} />
+          <Circle
+            center={currentLocation.position}
+            radius={currentLocation.accuracy}
+          />
+        </>
+      )}
       <Polyline positions={shape?.RoutePath || []} color={shape?.LineColor}>
         <Popup>{shape.RoutePath && <div>{shape.Names}</div>}</Popup>
       </Polyline>
