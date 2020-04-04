2b45731a1db8f3964434ea2d54fcc2ab682aa165
Fulton Garcia <fultongarcia@gmail.com>
2020-04-04 19:44:09 -0400

    Updates

---
+++ precache-manifest.90bc010cc007ce6f5f3061827c606c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5daf7251fb180bbf2f388aaa06d2743",
+    "url": "/GTFS-2020/index.html"
+  },
+  {
+    "revision": "dd86388bf588a8acc729",
+    "url": "/GTFS-2020/static/css/main.5c7c688c.chunk.css"
+  },
+  {
+    "revision": "026488cdea11295ec8d1",
+    "url": "/GTFS-2020/static/js/2.c8c07b3b.chunk.js"
+  },
+  {
+    "revision": "119b1af2874f43cb1268e979dcee0818",
+    "url": "/GTFS-2020/static/js/2.c8c07b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd86388bf588a8acc729",
+    "url": "/GTFS-2020/static/js/main.401cee95.chunk.js"
+  },
+  {
+    "revision": "bb22edec673673bb9ab8",
+    "url": "/GTFS-2020/static/js/runtime-main.adca50d0.js"
+  }
+]);
